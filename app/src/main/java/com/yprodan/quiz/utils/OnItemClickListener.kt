35576e9cc63fbc77131a33efa994d7d4489b1aa5
Yaroslav <yarik.rodan@gmail.com>
2020-11-09 23:30:16 +0200

    changed one comment

---
+++ app/src/main/java/com/yprodan/quiz/utils/OnItemClickListener.kt
@@ -0,0 +1,8 @@
+package com.yprodan.quiz.utils
+
+/**
+ * Created for Item RecyclerViewAdapter.ViewHolder to implement the onClick method
+ */
+interface OnItemClickListener {
+    fun onClick(fileName: String)
+}
